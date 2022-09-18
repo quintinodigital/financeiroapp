33aa7d8ec9cfdc10c14daee22da10bf8179ac72c
Palladhium <palladhium@outlook.com.br>
2022-09-18 14:31:29 -0300

    FINANCEIROAPP0214092022FEATURE
    - Implementação da Tela de Receita

--- src/app/page/receita/receita.page.ts
+++ src/app/page/receita/receita.page.ts
@@ -9,8 +9,49 @@ export class ReceitaPage implements OnInit {
 
   public isSemTransacao = true;
 
+  public nomeMes: String = "";
+  public numeroMesCorrente: any = 0;
+
+  public mesAnoList = [
+    { nome: "Janeiro" },
+    { nome: "Fevereiro" },
+    { nome: "Março" },
+    { nome: "Abril" },
+    { nome: "Maio" },
+    { nome: "Junho" },
+    { nome: "Julho" },
+    { nome: "Agosto" },
+    { nome: "Setembro" },
+    { nome: "Outubro" },
+    { nome: "Novembro" },
+    { nome: "Dezembro" }
+  ]
+
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this.nomeMes = this.mesAnoList[8].nome;
+    this.recuperarNumeroMesCorrente();
+  }
+
+  private recuperarNumeroMesCorrente() {
+    var dataAtual = new Date();
+    this.numeroMesCorrente = dataAtual.getMonth();
+  }
+
+  public recuperarMesAnterior() { 
+    var indice = this.numeroMesCorrente;
+    if(indice === this.numeroMesCorrente) {
+      this.numeroMesCorrente--;
+    }
+    if(indice >= 0 && indice) {
+      console.log(indice);
+      return this.nomeMes = this.mesAnoList[indice].nome;
+    }
+  }
+
+  public recuperarMesPosterior(indice: number) { 
+    return this.nomeMes = this.mesAnoList[this.numeroMesCorrente+indice].nome
+  }
 
 }
