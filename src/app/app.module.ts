53a5a33148cb096220ffe00168dfbc6cb296239a
Dysprosium <dysprosium@outlook.com.br>
2022-09-21 13:22:27 -0300

    FINANCEIROAPP0121092022FEATURE
    - Configurar os tipos monetários para o formato brasileiro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
+import { NgModule, LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+registerLocaleData(ptBr);
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+      { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, 
+      { provide: LOCALE_ID, useValue: 'pt' },
+      { provide:  DEFAULT_CURRENCY_CODE, useValue: 'BRL' }
+    ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
